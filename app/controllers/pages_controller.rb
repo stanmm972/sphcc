e828251864536140608a0b5da5250f75ff711290
fred maurice <fmmadelon@gmail.com>
2021-03-02 12:18:20 -0500

    Create pages and controller for pages and make app production ready

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def qui
+    end
+
+    def partenaire
+    end
+
+    def journal
+    end
+
+    def dons
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'qui', to: 'pages#qui'
+  get 'partenaire', to: 'pages#partenaire'
+  get 'journal', to: 'pages#journal'
+  get 'dons', to: 'pages#dons'
+end
